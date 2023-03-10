74fbfa91e9f773ddd51242c6fe59878a0a4a9063
kulikovm11 <maksym.obo@gmail.com>
2023-03-10 18:49:08 +0200

    Add tailwind

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ const App = () => {
     return (
         <div>
             Hello future shop :)
+            <button className="py-2 px-4 border hover:bg-sky-700 rounded-xl">Click me</button>
         </div>
     );
 };
